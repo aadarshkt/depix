c8cde0437876ba946f1fa35ed87d74ff07384e51
Kartik <kartikhatwar98@gmail.com>
2022-05-07 23:13:05 +0530

    First Commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,60 +1,197 @@
-import { Box, Typography, IconButton, Button } from "@mui/material";
+import { Box, Typography, IconButton, Button, AppBar, Toolbar } from "@mui/material";
 import React from "react";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
+import Home from '@mui/icons-material/Home';
+import CategoryIcon from '@mui/icons-material/Category';
+
+
 
 const Header = ({ handleClickOpen, walletAddress, connectWalletPressed }) => {
 
   
+
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        p: 2,
-      }}
-    >
-      <Box
+
+    <React.Fragment>
+      <AppBar sx ={{
+        background:'#fff7',
+      }}>
+        
+        <Toolbar>
+        <Box m={1}
         sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "flex-start",
-          width: "50%",
-          height: 50,
-          backgroundColor: "white",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" color="grey">
-          Depix
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "flex-end",
-          width: "50%",
-          height: 50,
-          backgroundColor: "white",
-          alignItems: "center",
-        }}
-      >
-        <IconButton aria-label="create post" onClick={handleClickOpen}>
-          <AddCircleOutlineRoundedIcon fontSize="large" color="white" />
-        </IconButton>
-        <Button variant="outlined" onClick={connectWalletPressed}>
-          {" "}
-          {walletAddress.length > 0 ? (
-            "Connected: " +
-            String(walletAddress).substring(0, 6) +
-            "..." +
-            String(walletAddress).substring(38)
-          ) : (
-            <span>Connect Wallet</span>
-          )}
-        </Button>
-      </Box>
-    </Box>
+          
+        }}>
+
+             <Typography variant="h4" color="#072953" fontSize="2.125rem">
+              DivMiners
+            </Typography>
+        </Box>
+
+        <Box ml={2}
+           sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            width: "50%",
+            height: 50,
+            fontSize:"2rem",
+            alignContent: "center",
+            backgroundColor: "inherit",
+            alignItems: "center",
+          }}
+          >  
+                      <Box>
+        
+                       <Button sx={{ color:"#128cffa3",
+                        fontSize:"1.4rem",
+                        }} mr={1} ml={1}>
+                        <Home sx={{ 
+                        color:"#574ff4",
+                        width:"2rem",
+                        height:"2rem",
+                         }} mr={1} ml={1}/>
+
+                      <Box ml={1}> 
+                         Home
+                      </Box> </Button>
+
+                      </Box>
+                      <Box>
+        
+                       <Button sx={{ color:"#128cffa3", 
+                       fontSize:"1.4rem"}}>
+                        <CategoryIcon sx={{ 
+                        color:"#574ff4",
+                        width:"2rem",
+                        height:"2rem",
+                         }} mr={1} ml={1}/>
+
+                      <Box ml={1}> 
+                          Category
+                      </Box> </Button>
+
+                      </Box>
+    
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              width: "50%",
+              height: 50,
+              backgroundColor: "white",
+              alignItems: "center",
+            }}
+          >
+            <IconButton aria-label="create post" onClick={handleClickOpen}>
+              <AddCircleOutlineRoundedIcon fontSize="large" color="white" />
+            </IconButton>
+            <Button color="info" ml={2} variant="contained" onClick={connectWalletPressed}>
+              {" "}
+              {walletAddress.length > 0 ? (
+                "Connected: " +
+                String(walletAddress).substring(0, 6) +
+                "..." +
+                String(walletAddress).substring(38)
+              ) : (
+                <span>Connect Wallet</span>
+              )}
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+
+    
+
+    // <Box
+    //   sx={{
+    
+    //     display: "flex",
+    //     flexDirection: "row",
+    //     p: 2,
+    //   }}
+    // >
+
+    //       <Box
+    //         sx={{
+    //           display: "flex",
+    //           flexDirection: "row",
+    //           justifyContent: "flex-start",
+    //           width: "50%",
+    //           height: 50,
+    //           backgroundColor: "white",
+    //           alignItems: "center",
+    //         }}
+    //       >
+    //         <Typography variant="h4" color="grey">
+    //           Depix
+    //         </Typography>
+    //       </Box>
+    //       <Box
+    //        sx={{
+    //         display: "flex",
+    //         flexDirection: "row",
+    //         justifyContent: "flex-start",
+    //         width: "50%",
+    //         height: 50,
+    //         alignContent: "center",
+    //         backgroundColor: "white",
+    //         alignItems: "center",
+    //       }}
+    //       >    <Home/>
+
+    //        <Button
+    //        style={homeStyle}>Home</Button>
+
+    //       </Box>
+
+    //       <Box
+    //        sx={{
+    //         display: "flex",
+    //         flexDirection: "row",
+    //         justifyContent: "flex-start",
+    //         width: "50%",
+    //         height: 50,
+    //         backgroundColor: "white",
+    //         alignItems: "center",
+    //       }}
+    //       >    <CategoryIcon/>  
+          
+    //        <Button>Category</Button>
+
+    //       </Box>
+
+    //       <Box
+    //         sx={{
+    //           display: "flex",
+    //           flexDirection: "row",
+    //           justifyContent: "flex-end",
+    //           width: "50%",
+    //           height: 50,
+    //           backgroundColor: "white",
+    //           alignItems: "center",
+    //         }}
+    //       >
+    //         <IconButton aria-label="create post" onClick={handleClickOpen}>
+    //           <AddCircleOutlineRoundedIcon fontSize="large" color="white" />
+    //         </IconButton>
+    //         <Button variant="outlined" onClick={connectWalletPressed}>
+    //           {" "}
+    //           {walletAddress.length > 0 ? (
+    //             "Connected: " +
+    //             String(walletAddress).substring(0, 6) +
+    //             "..." +
+    //             String(walletAddress).substring(38)
+    //           ) : (
+    //             <span>Connect Wallet</span>
+    //           )}
+    //         </Button>
+    //       </Box>
+    // </Box>
   );
 };
 
